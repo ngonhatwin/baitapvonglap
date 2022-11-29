ca74b120287475bffee38cde51e091635c1c8bfb
NgoNhatWin <117475856+ngonhatwin@users.noreply.github.com>
2022-11-29 08:39:04 +0700

    Add files via upload

---
+++ baitap2.cpp
@@ -0,0 +1,17 @@
+#include "stdio.h"
+int main () {
+	
+	int n,tong ;
+	tong = 0;
+	n=0;
+	while ( tong < 10000) {
+		tong = tong + n;
+		n++;
+		
+	}
+	printf ("\n so nguyen duong n nho nhat sao cho 1+2+... +n <10000 la = %d ", n);
+	printf ("\n tong cac so tu 1 den n la = %d", tong );
+}
+
+
+
